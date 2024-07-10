c96a6af097e26c9875dc6d212e6c55d2732cc40a
TedMartell <martellted@gmail.comgit config --global user.name TedMartell$ git config --global user.name John>
2024-07-10 13:34:49 +0300

    handlers added

---
+++ handler.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++ // Increment the counter
+		next.ServeHTTP(w, r) // Call the next handler in the chain
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	formattedHits := fmt.Sprintf("Hits: %v", cfg.fileserverHits)
+	w.Write([]byte(formattedHits))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits = 0
+	w.Write([]byte("fileserverHits has been reset to 0"))
+}
--- main.go
+++ main.go
@@ -9,8 +9,20 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	// Initialize apiConfig
+	apiCfg := &apiConfig{}
+
+	// Set up a file server handler
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	// Set up the serve mux and wrap the file server with the middleware
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/healthz", http.HandlerFunc(handlerReadiness))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	// Add `mux.Handle` for `/metrics` and `/reset`
 
 	srv := &http.Server{
 		Addr:    ":" + port,
