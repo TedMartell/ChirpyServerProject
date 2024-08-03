ce46696f4daab8884d5ed272c3fecdf99e89e271
TedMartell <martellted@gmail.comgit config --global user.name TedMartell$ git config --global user.name John>
2024-08-03 21:07:32 +0300

    Optional AuthorID to GET chirps added

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"sort"
 	"strconv"
+
+	"github.com/TedMartell/ChirpyServerProject/internal/database"
 )
 
 func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
@@ -27,20 +29,45 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
+	s := r.URL.Query().Get("author_id")
+
 	dbChirps, err := cfg.DB.GetChirps()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
 		return
 	}
 
-	chirps := []Chirp{}
-	for _, dbChirp := range dbChirps {
-		chirps = append(chirps, Chirp{
-			ID:   dbChirp.ID,
-			Body: dbChirp.Body,
-		})
+	chirps := []database.Chirp{}
+	if s != "" {
+		// Convert author_id to integer
+		authorID, err := strconv.Atoi(s)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID")
+			return
+		}
+
+		// Filter based on author_id
+		for _, dbChirp := range dbChirps {
+			if dbChirp.AuthorID == authorID {
+				chirps = append(chirps, database.Chirp{
+					ID:       dbChirp.ID,
+					AuthorID: dbChirp.AuthorID,
+					Body:     dbChirp.Body,
+				})
+			}
+		}
+	} else {
+		// Otherwise, include all chirps
+		for _, dbChirp := range dbChirps {
+			chirps = append(chirps, database.Chirp{
+				ID:       dbChirp.ID,
+				AuthorID: dbChirp.AuthorID,
+				Body:     dbChirp.Body,
+			})
+		}
 	}
 
+	// Sort by ID in ascending order
 	sort.Slice(chirps, func(i, j int) bool {
 		return chirps[i].ID < chirps[j].ID
 	})
--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
-	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
+	mux.HandleFunc("GET /api/chirps/", apiCfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
 	mux.HandleFunc("DELETE /api/chirps/{chirpID}", apiCfg.handlerChirpsDelete)
 
